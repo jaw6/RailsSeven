fc4831f39c86acbae4d350423b9bee466ea4d808
yheuhtozr <84892012+yheuhtozr@users.noreply.github.com>
2023-04-12 23:23:23 +0900

    set up i18n and test case

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
   def default_url_options(**options)
     { locale: params[:locale].presence }
+  end
+
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
