4c1d4bae04b259a0b8d21c36ae1bc22be9fa9c86
Joshua Wehner <joshua@forem.com>
2023-04-12 14:24:50 +0200

    Add default locale route options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def default_url_options(**options)
+    { locale: params[:locale].presence }
+  end
 end
---
+++ app/controllers/whatever_controller.rb
@@ -0,0 +1,4 @@
+class WhateverController < ApplicationController
+  def show
+  end
+end
